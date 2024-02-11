51d99927d7cf0d8730ed6a4d65b6ce4d6a398e9c
benwsheridan <ben.w.sheridan@gmail.com>
2024-02-11 18:16:17 +0000

    rebuild fixes

--- containers/rebuild.ps1
+++ containers/rebuild.ps1
@@ -1,4 +1,5 @@
 # pasteable
+podman machine start
 podman rm -f bernso_container
 podman build -f .\Containerfile -t bernso:latest
 podman run --rm -d -p 8080:80 --name bernso_container bernso:latest
