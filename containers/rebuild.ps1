7ee470a19850ad438a092abf5082afc8297741b6
benwsheridan <ben.w.sheridan@gmail.com>
2024-02-11 21:29:24 +0000

    made the ReBuild poweshell fancy

--- containers/rebuild.ps1
+++ containers/rebuild.ps1
@@ -1,8 +1,24 @@
 # pasteable
+
 $mypath = $MyInvocation.MyCommand.Path
 $MyNewPath = Split-Path $mypath -Parent
 Set-Location $MyNewPath
-podman machine start
+
+
+$stateInfo = podman machine info --format json | convertfrom-json
+
+if ($stateInfo.Host.MachineState -eq "Running"){
+    Write-Host -foregroundcolor Green 'Podman-machine running'
+}
+
+if ($stateInfo.Host.MachineState -ne "Running"){
+    Write-Host -foregroundcolor yellow 'Podman-machine not running: Staring-up'
+    podman machine start
+    $stateInfo = podman machine info --format json | convertfrom-json
+    Write-Host -foregroundcolor green 'Podman-machine' $stateInfo.Host.MachineState
+}
+
+
 podman rm -f bernso_container
 podman build -f .\Containerfile -t bernso:latest
 podman run --rm -d -p 8080:80 --name bernso_container bernso:latest
